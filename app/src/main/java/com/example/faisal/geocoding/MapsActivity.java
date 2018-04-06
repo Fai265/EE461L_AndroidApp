d337eed83e7319a1ce4d91456d063d8754710639
fai265 <faisalm1122@gmail.com>
2018-04-06 15:32:29 -0500

    Have a map ready

--- app/src/main/java/com/example/faisal/geocoding/MapsActivity.java
+++ app/src/main/java/com/example/faisal/geocoding/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.faisal.geocoding;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.webkit.PermissionRequest;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -42,5 +43,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        try {
+            mMap.setMyLocationEnabled(true);
+        }
+        catch(SecurityException e){
+
+        }
     }
 }
